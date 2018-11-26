c9f7e82e991ac1af9df07be8e3cdca779d2e3455
Tom Coldenhoff <tom.coldenhoff@outlook.com>
2018-11-26 08:51:49 +0100

    Added function in world class for better WorldObject initializing and Registering

--- TomFrame/src/TomFrame/Game/World/GameWorld.h
+++ TomFrame/src/TomFrame/Game/World/GameWorld.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "World.h"
 #include <vector>
+#include <SFML/System/Vector2.hpp>
+#include "../Objects/WorldObject.h"
 #include "../../Debug/Logger.h"
 
 namespace TomFrame 
 {
-	class WorldObject;
 	class GameWorld : public World 
 	{
 	public:
@@ -18,6 +19,18 @@ namespace TomFrame
 				return;
 			}
 			m_WorldObjects.push_back(object);
+		}
+
+		/*Initializes an object and registers it to this world*/
+		virtual void CreateAndRegister(WorldObject* object, std::string spritePath, sf::Vector2f spawnLocation)
+		{
+			if (object == nullptr)
+			{
+				TomFrame::Debug::Logger::LogError("object is nullptr. FILE: GameWorld.h, LINE: 29");
+			}
+
+			object->Initialise(spawnLocation, spritePath);
+			RegisterObject(object);
 		}
 
 		/*Updates and draws all the registered objects*/
--- TomFrame/src/TomFrame/Game/World/World.h
+++ TomFrame/src/TomFrame/Game/World/World.h
@@ -1,5 +1,15 @@
 #pragma once
 #include "../../Core.h"
+#include <string>
+
+//Forward declaration of sf namespace classes
+namespace sf
+{
+	template<typename T>
+	class Vector2;
+
+	typedef Vector2<float> Vector2f;
+}
 
 namespace TomFrame 
 {
@@ -12,5 +22,7 @@ namespace TomFrame
 	public:
 
 		virtual void RegisterObject(WorldObject* object) = 0;
+		virtual void CreateAndRegister(WorldObject* object, std::string spritePath, sf::Vector2f spawnLocation) = 0;
 	};
+
 }
