01f4bfcdcd1ec77c63483d4acccbdb56a7006d68
Tom Coldenhoff <tom.coldenhoff@outlook.com>
2018-12-07 11:45:34 +0100

    Bug speed

--- Game/src/Game/Bullet.cpp
+++ Game/src/Game/Bullet.cpp
@@ -1,9 +1,19 @@
 #include "Bullet.h"
 #include "TomFrame/Game/Components/TransfromComponent.h"
+#include "TomFrame/WindowSettings.h"
 
 
 void Bullet::Update(float deltaTime)
 {
+	
 	GetTransformComponent()->Move(sf::Vector2f(400.0f, 0.0f) * deltaTime);
+
+	//Destroy bullet when out of screen
+	if (GetTransformComponent()->GetPosition().x > GetWorld()->GetWindowSettings()->GetScreenSize().Width)
+	{
+		GetWorld()->DestroyWorldObject(this);
+	}
+
+	TomFrame::Debug::Logger::Log(deltaTime);
 }
 
